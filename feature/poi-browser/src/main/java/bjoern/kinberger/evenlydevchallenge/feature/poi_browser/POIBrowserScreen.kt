2dcbeaef14b30eae62aabf2fff84a4d6ffbcaede
bjoern.kinberger <bjoern.kinberger@grandcentrix.net>
2024-12-10 13:55:17 +0100

    beautify layout

--- feature/poi-browser/src/main/java/bjoern/kinberger/evenlydevchallenge/feature/poi_browser/POIBrowserScreen.kt
+++ feature/poi-browser/src/main/java/bjoern/kinberger/evenlydevchallenge/feature/poi_browser/POIBrowserScreen.kt
@@ -1,18 +1,23 @@
 package bjoern.kinberger.evenlydevchallenge.feature.poi_browser
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import bjoern.kinberger.evenlydevchallenge.data.nearby_places.model.NearbyPlace
@@ -28,7 +33,6 @@ internal fun POIBrowserScreen(
     LazyColumn(
         modifier = modifier,
     ) {
-
         when (uiState) {
             POIBrowserUiState.Error -> item {
                 Text("Something went wrong!")
@@ -53,25 +57,46 @@ internal fun POIBrowserScreen(
 private fun POICell(
     poi: NearbyPlace
 ) {
-    var showLocationDetails by rememberSaveable { mutableStateOf(false) }
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(8.dp)
-            .clickable {
-                showLocationDetails = !showLocationDetails
-            },
+            .padding(8.dp),
+        colors = CardColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = Color.White,
+            disabledContentColor = Color.White,
+            disabledContainerColor = MaterialTheme.colorScheme.secondary
+        )
     ) {
-        Column(
-            modifier = Modifier.padding(8.dp)
-        ) {
-            Text(poi.name)
-            Text(poi.categoryName)
+        Row(modifier = Modifier.padding(8.dp)) {
+            Box(
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(MaterialTheme.colorScheme.primaryContainer)
 
-            if (showLocationDetails) {
-                Text("${poi.location.street}, ${poi.location.city} ${poi.location.postcode}")
+            )
+            Column(
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+            ) {
+                Text(
+                    text = poi.name,
+                    style = MaterialTheme.typography.titleMedium
+                )
+                Text(
+                    text = poi.categoryName,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+
+                Text(
+                    text = "${poi.location.street}, ${poi.location.city} ${poi.location.postcode}",
+                    style = MaterialTheme.typography.bodyMedium
+                )
+
             }
         }
+
 
     }
 }
